ae2a4c865d877d83afc839b791ef8304a80adae1
benji_mac <benaja.bendo02@gmail.com>
2024-02-21 08:36:02 +0100

    - add method register - add route register - add feat register

--- src/utils/config-routes.ts
+++ src/utils/config-routes.ts
@@ -1,6 +1,7 @@
 export interface Route {
     login: string;
     logout: string;
+    register: string;
     plants: {
         getAll: string;
         show: (id: number) => string;
@@ -10,6 +11,7 @@ export interface Route {
 const configRoutes: Route = {
     login: import.meta.env.VITE_API_ROUTE_LOGIN || '/login',
     logout: import.meta.env.VITE_API_ROUTE_LOGOUT || '/logout',
+    register: import.meta.env.VITE_API_ROUTE_REGISTER || '/register',
     plants: {
         getAll: import.meta.env.VITE_API_ROUTE_PLANTS_GET_ALL || '/plants',
         show: (id: number) =>`/plants/${id}`
